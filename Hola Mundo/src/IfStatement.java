6378b7123856d30a094186ce3245bd827817aa36
Masterick32 <123507362+Masterick32@users.noreply.github.com>
2023-06-07 16:24:00 -0500

    Actualizado if statement excercise

---
+++ Hola Mundo/src/IfStatement.java
@@ -0,0 +1,11 @@
+public class IfStatement {
+    public static void main(String[] args) {
+        boolean isBluetoothEnabled = true;
+        int fileSended = 3;
+
+        if (isBluetoothEnabled){
+            fileSended++;
+            System.out.println("Archivo Enviado");
+        }
+    }
+}
